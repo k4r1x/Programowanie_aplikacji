fa000684d813d63ac94afdc9f2124af13eac69b1
k4r1x <karol07wp.pl@gmail.com>
2022-11-24 10:30:44 +0100

    24 listopad

--- T13 Biblioteka_p5js/sketch.js
+++ T13 Biblioteka_p5js/sketch.js
@@ -5,26 +5,46 @@ function setup() {
 
 function draw() {
     background(200);
-    rotateX(frameCount * -0.01);
-    rotateY(frameCount * -0.01);
-    box(100);
-    rotateX(frameCount * 0.01);
-    rotateY(frameCount * 0.01);
-    translate(0,100);
-    box(100);
-    translate(100,0);
-    box(100);
-    translate(0,-100);
-    box(100);
-    translate(0,-100);
-    box(100);
-    translate(-100,0);
-    box(100);
-    translate(-100,0);
-    box(100);
-    translate(0,100);
-    box(100);
-    translate(0,100);
-    box(100);
-
+    if(mouseIsPressed) {
+        rotateX(frameCount * -0.01);
+        rotateY(frameCount * -0.01);
+        box(100);
+        rotateX(frameCount * 0.01);
+        rotateY(frameCount * 0.01);
+        translate(0, 100);
+        box(100);
+        translate(100, 0);
+        box(100);
+        translate(0, -100);
+        box(100);
+        translate(0, -100);
+        box(100);
+        translate(-100, 0);
+        box(100);
+        translate(-100, 0);
+        box(100);
+        translate(0, 100);
+        box(100);
+        translate(0, 100);
+        box(100);
+    }
+    else{
+        box(100);
+        translate(0, 100);
+        box(100);
+        translate(100, 0);
+        box(100);
+        translate(0, -100);
+        box(100);
+        translate(0, -100);
+        box(100);
+        translate(-100, 0);
+        box(100);
+        translate(-100, 0);
+        box(100);
+        translate(0, 100);
+        box(100);
+        translate(0, 100);
+        box(100);
+    }
 }
