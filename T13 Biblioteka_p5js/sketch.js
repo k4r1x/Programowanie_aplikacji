c5e6c13178e9f50197489e74dc5c0f0bc151d692
k4r1x <89997113+k4r1x@users.noreply.github.com>
2022-11-27 19:19:24 +0100

    Add files via upload

--- T13 Biblioteka_p5js/sketch.js
+++ T13 Biblioteka_p5js/sketch.js
@@ -1,50 +1,37 @@
 function setup() {
-    createCanvas(1600, 750, WEBGL);
-    normalMaterial();
-}
+    createCanvas(1900, 1000, WEBGL);
+  }
+  
+  function draw() {
+    background(0);
 
-function draw() {
-    background(200);
-    if(mouseIsPressed) {
-        rotateX(frameCount * -0.01);
-        rotateY(frameCount * -0.01);
-        box(100);
-        rotateX(frameCount * 0.01);
-        rotateY(frameCount * 0.01);
-        translate(0, 100);
-        box(100);
-        translate(100, 0);
-        box(100);
-        translate(0, -100);
-        box(100);
-        translate(0, -100);
-        box(100);
-        translate(-100, 0);
-        box(100);
-        translate(-100, 0);
-        box(100);
-        translate(0, 100);
-        box(100);
-        translate(0, 100);
-        box(100);
-    }
-    else{
-        box(100);
-        translate(0, 100);
-        box(100);
-        translate(100, 0);
-        box(100);
-        translate(0, -100);
-        box(100);
-        translate(0, -100);
-        box(100);
-        translate(-100, 0);
-        box(100);
-        translate(-100, 0);
-        box(100);
-        translate(0, 100);
-        box(100);
-        translate(0, 100);
-        box(100);
-    }
+    let locX = mouseX - height / 2;
+    let locY = mouseY - width / 2;
+  
+    ambientLight(120);
+    directionalLight(255, 100, 0, 0.25, 0.70, 0);
+    pointLight(0, 100, 255, locX, locY, 250);
+  
+    translate(width / 100000, -170, 0);
+    rotateZ(frameCount * 0.02);
+    rotateX(frameCount * 0.02);
+    ambientMaterial(250);
+    sphere(150, 64);
+
+    push();
+    translate(-width / 7, 0, 0);
+    rotateZ(frameCount * 0.02);
+    rotateX(frameCount * 0.02);
+    specularMaterial(250);
+    sphere(50, 64);
+    pop();
+  
+    point(300, 200);
+    stroke('white');
+    strokeWeight(10);
+
+    point(350, 250);
+    stroke('white');
+    strokeWeight(10);
+    
 }
