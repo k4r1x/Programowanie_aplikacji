97f2fea3a8e09b8f3e08d2b17e3111a5a5948954
k4r1x <karol07wp.pl@gmail.com>
2023-01-02 10:05:11 +0100

    BMI

--- 103 BMI/script.js
+++ 103 BMI/script.js
@@ -12,14 +12,14 @@ btn.addEventListener('click', function(){
 function BMI(waga, wzrost){
     let bmi = waga / Math.pow(wzrost, 2);
     switch(true){
-        case (bmi < 16): return "Wygłodzenie"; break;
-        case (bmi >= 16 && bmi <= 16.99): return "Wychudzenie"; break;
-        case (bmi >= 17 && bmi <= 18.49): return "Niedowaga"; break;
-        case (bmi >= 18.5 && bmi <= 24.99): return "Optimum"; break;
-        case (bmi >=25 && bmi <=29.99): return "Nadwaga"; break;
-        case (bmi >= 30 && bmi <= 34.99): return "Otyłość I st."; break;
-        case (bmi >= 35 && bmi <= 39.99): return "Otyłość II st."; break;
-        case (bmi >= 40): return "Otyłość III st."; break;
+        case (bmi < 16): return "Wygłodzenie";
+        case (bmi >= 16 && bmi <= 16.99): return "Wychudzenie";
+        case (bmi >= 17 && bmi <= 18.49): return "Niedowaga";
+        case (bmi >= 18.5 && bmi <= 24.99): return "Optimum";
+        case (bmi >=25 && bmi <=29.99): return "Nadwaga";
+        case (bmi >= 30 && bmi <= 34.99): return "Otyłość I st.";
+        case (bmi >= 35 && bmi <= 39.99): return "Otyłość II st.";
+        case (bmi >= 40): return "Otyłość III st.";
         default: return "Nieprawidłowe dane!";
     }
 }
